b88ad340a8695ec2b9965c88c610cc538e9741f9
akcholpon <kimhyungni>
2023-11-22 08:08:49 +0600

    js

--- js/core.js
+++ js/core.js
@@ -1,4 +1,4 @@
 document.querySelector('.js_btn').addEventListener('click', function(){
     document.querySelector('.js_btn').classList.add('active')
-    document.querySelector('.js_btn').65ry
+    document.querySelector('.js_btn').
 })  
