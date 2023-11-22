f50675740c20656a6316bc9e17013ea2d44c4475
akcholpon <kimhyungni>
2023-11-22 08:21:50 +0600

    footer

--- js/core.js
+++ js/core.js
@@ -1,4 +1,4 @@
 document.querySelector('.js_btn').addEventListener('click', function(){
     document.querySelector('.js_btn').classList.add('active')
-    document.querySelector('.js_btn').
+    document.querySelector('.js_btn')
 })  
